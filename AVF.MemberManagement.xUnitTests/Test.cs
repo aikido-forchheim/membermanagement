76fac7d298b45217b9e66d733333e60170f686e6
Markus Malz <asgart79@gmail.com>
2017-05-12 09:46:22 +0200

    added xUnit project

---
+++ AVF.MemberManagement.xUnitTests/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xunit;
+namespace AVF.MemberManagement.xUnitTests
+{
+    public class Test
+    {
+        [Fact()]
+        public void TestMethod()
+        {
+        }
+    }
+}
