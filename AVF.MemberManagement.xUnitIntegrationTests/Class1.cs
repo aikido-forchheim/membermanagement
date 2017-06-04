369e42146fa183dddc0027a07c3134c87a1e3d45
Markus Malz <markus.malz@outlook.com>
2017-06-04 16:14:49 +0200

    now start a basic test

--- AVF.MemberManagement.xUnitIntegrationTests/Class1.cs
+++ AVF.MemberManagement.xUnitIntegrationTests/Class1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AVF.MemberManagement.xUnitIntegrationTests
 {
     public class Class1
     {
+        [Fact()]
+        public void StartBasicTest()
+        {
+            var couldLogin = false;
+
+
+
+            Assert.True(couldLogin);
+        }
     }
 }
