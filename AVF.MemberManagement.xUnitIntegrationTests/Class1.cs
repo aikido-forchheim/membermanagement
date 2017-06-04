848d948ad9c5ad5a58cf15b7d61c51c67431a942
Markus Malz <markus.malz@outlook.com>
2017-06-04 17:22:11 +0200

    IntegrationTestSettings

--- AVF.MemberManagement.xUnitIntegrationTests/Class1.cs
+++ AVF.MemberManagement.xUnitIntegrationTests/Class1.cs
@@ -1,20 +1,37 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AVF.MemberManagement.StandardLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using Xunit;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AVF.MemberManagement.xUnitIntegrationTests
 {
     public class Class1
     {
+        //[Fact()]
+        //public void WriteIntegrationTestSettings()
+        //{
+        //    var sampleSettings = new IntegrationTestSettings();
+        //    var sampleSettingsToWrite = JsonConvert.SerializeObject(sampleSettings);
+
+        //    File.WriteAllText("C:\\temp\\IntegrationTestSettings.json", sampleSettingsToWrite, Encoding.UTF8);
+        //}
+
         [Fact()]
         public void StartBasicTest()
         {
-            var couldLogin = false;
+            bool couldLogin;
 
+            var integrationTestSettings = IntegrationTestSettings.Get();
 
+            couldLogin = integrationTestSettings.RestApiAccount.Username != "username";
 
             Assert.True(couldLogin);
         }
--- AVF.MemberManagement.xUnitIntegrationTests/IntegrationTestSettings.cs
+++ AVF.MemberManagement.xUnitIntegrationTests/IntegrationTestSettings.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AVF.MemberManagement.StandardLibrary.Models;
+using Newtonsoft.Json;
 
 namespace AVF.MemberManagement.xUnitIntegrationTests
 {
     public class IntegrationTestSettings
     {
         public RestApiAccount RestApiAccount { get; set; } = new RestApiAccount() { Username = "username", Password = "password", ApiUrl = "https://www.homepage.com/api.php"};
+        
+        public static IntegrationTestSettings Get()
+        {
+            var pathToIntegrationTestSettings =
+                Environment.GetEnvironmentVariable(
+                    "AVF.MemberManagement.xUnitIntegrationTests.IntegrationTestSettings");
+
+            IntegrationTestSettings integrationTestSettings;
+
+            if (pathToIntegrationTestSettings != null && File.Exists(pathToIntegrationTestSettings))
+            {
+                var json = File.ReadAllText(pathToIntegrationTestSettings, Encoding.UTF8);
+
+                integrationTestSettings = JsonConvert.DeserializeObject<IntegrationTestSettings>(json);
+            }
+            else
+            {
+                throw new Exception("Please set environment variable with path to settings: AVF.MemberManagement.xUnitIntegrationTests.IntegrationTestSettings");
+            }
+
+            return integrationTestSettings;
+        }
     }
 }
