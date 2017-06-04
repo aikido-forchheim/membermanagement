495bc05c873586d58e758f515586ad0818a1aa73
Markus Malz <markus.malz@outlook.com>
2017-06-04 16:20:01 +0200

    IOptions

---
+++ AVF.MemberManagement.xUnitIntegrationTests/IntegrationTestSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVF.MemberManagement.xUnitIntegrationTests
+{
+    public class IntegrationTestSettings
+    {
+    }
+}
