4647aa1057858f408bda74a1d0d549a9cdc6dda5
Markus Malz <markus.malz@outlook.com>
2017-06-04 13:45:54 +0200

    Added SettingsProxy and ILogger

---
+++ AVF.MemberManagement.StandardLibrary/Models/SettingsWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVF.MemberManagement.StandardLibrary.Models
+{
+    internal class SettingsWrapper
+    {
+        public List<Setting> Settings { get; set; }
+    }
+}
---
+++ AVF.MemberManagement.StandardLibrary/Proxies/SettingsProxy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AVF.MemberManagement.StandardLibrary.Interfaces;
+using AVF.MemberManagement.StandardLibrary.Models;
+using Microsoft.Extensions.Logging;
+
+namespace AVF.MemberManagement.StandardLibrary.Proxies
+{
+    public class SettingsProxy : ISettingsProxy
+    {
+        private readonly ILogger _logger;
+        private readonly IPhpCrudApiService _phpCrudApiService;
+
+        private List<Setting> _settings = null;
+
+        public SettingsProxy(ILogger logger, IPhpCrudApiService phpCrudApiService)
+        {
+            _logger = logger;
+            _phpCrudApiService = phpCrudApiService;
+        }
+
+        public async Task<Setting> GetSettingAsync(string key)
+        {
+            if (_settings == null) await GetSettingsAsync();
+
+            var setting = (from s in _settings where s.Key == key select s).SingleOrDefault();
+
+            return setting;
+        }
+
+        public async Task<List<Setting>> GetSettingsAsync()
+        {
+            if (_settings != null) return _settings;
+
+            var uri = $"Settings";
+
+            _settings = (await _phpCrudApiService.GetDataAsync<SettingsWrapper>(uri)).Settings;
+
+            _logger.LogInformation(_settings.Count + " Settings loaded");
+
+            return _settings;
+        }
+    }
+}
