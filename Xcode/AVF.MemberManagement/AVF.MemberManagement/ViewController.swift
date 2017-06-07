60d64a7bb0d1aa2ebe34fb5394b28ba73d67b2b5
Markus Malz <asgart79@gmail.com>
2017-06-07 11:50:21 +0200

    Xcode project for creating provisioning profile

---
+++ Xcode/AVF.MemberManagement/AVF.MemberManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVF.MemberManagement
+//
+//  Created by Malz Markus on 07.06.17.
+//  Copyright © 2017 Malz Markus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
