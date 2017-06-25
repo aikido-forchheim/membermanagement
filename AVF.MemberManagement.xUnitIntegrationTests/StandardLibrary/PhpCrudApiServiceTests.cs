9c682428fbc22bef2f76e1406e4e84adb06f0503
Markus Malz <markus.malz@outlook.com>
2017-06-25 07:32:48 +0200

    PhpCrudApiServiceTests

---
+++ AVF.MemberManagement.xUnitIntegrationTests/StandardLibrary/PhpCrudApiServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AVF.MemberManagement.StandardLibrary.Interfaces;
+using Xunit;
+using Microsoft.Practices.Unity;
+
+namespace AVF.MemberManagement.xUnitIntegrationTests.StandardLibrary
+{
+    public class PhpCrudApiServiceTests : TestBase
+    {
+        [Fact]
+        public void GetObjectWithoutWrapper()
+        {
+            var phpCrudApiService = Bootstrapper.Container.Resolve<IPhpCrudApiService>();
+            //phpCrudApiService.
+        }
+    }
+}
