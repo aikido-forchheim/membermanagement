639906294817ea1758c5cb60c07fcd6f4ab5de00
Markus Malz <markus.malz@outlook.com>
2017-11-11 19:14:40 +0100

    seems ok

--- AVF.MemberManagement.Console/StundensatzKalkulator.cs
+++ AVF.MemberManagement.Console/StundensatzKalkulator.cs
@@ -17,22 +17,39 @@ namespace AVF.MemberManagement.Console
         {
             var trainings = await Program.Container.Resolve<IRepository<Training>>().GetAsync();
             var mitglieder = await Program.Container.Resolve<IRepository<Mitglied>>().GetAsync();
+            var trainerErnennungen = await Program.Container.Resolve<IRepository<TrainerErnennung>>().GetAsync();
+            var stundensaetze = await Program.Container.Resolve<IRepository<Stundensatz>>().GetAsync();
 
             var trainingsJanuar = trainings.Where(training =>
                 training.Termin > new DateTime(2016, 01, 01) && training.Termin < new DateTime(2016, 02, 01)).ToList();
 
-            var trainierJanuar = trainingsJanuar.Select(training => training.Trainer).Distinct().ToList();
+            var hauptTrainierJanuar = trainingsJanuar.Select(training => training.Trainer).Distinct().ToList();
 
-            foreach (var trainerId in trainierJanuar)
+            foreach (var trainerId in hauptTrainierJanuar)
             {
-                var trainingsTrainerX = trainingsJanuar.Where(t => t.Trainer == trainerId).ToList();
+                if (trainerId == -1) continue;
 
-                var minutenGesamt = trainingsTrainerX.Sum(t => t.DauerMinuten);
+                var trainingsTrainerX = trainingsJanuar.Where(t => t.Trainer == trainerId).ToList();
 
                 var name = mitglieder.Single(m => m.Id == trainerId).Nachname;
                 var vorname = mitglieder.Single(m => m.Id == trainerId).Vorname;
 
-                System.Console.WriteLine($"{vorname} {name}: {minutenGesamt}");
+
+                decimal entgelt = 0;
+                var minutenGesamt = 0;
+
+                foreach (var training in trainingsTrainerX)
+                {
+                    minutenGesamt += training.DauerMinuten;
+
+                    var trainererstufe = trainerErnennungen.Where(t => t.MitgliedID == trainerId).Min(t => t.StufeID);
+
+                    var stundensatz = stundensaetze.Single(s => s.TrainerStufenID == trainererstufe && s.TrainerNummer == 1 && s.Dauer == training.DauerMinuten); //s.TrainerNummer == 1 => Haupttrainer
+
+                    entgelt += stundensatz.Betrag;
+                }
+
+                System.Console.WriteLine($"{vorname} {name}: {minutenGesamt}, {entgelt}");
             }
         }
     }
