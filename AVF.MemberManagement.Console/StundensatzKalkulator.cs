1cb958ba97d5523c4e5eb240e0de6b60ed35005b
Peter Kraus <peter.kraus54@outlook.de>
2017-11-20 01:47:10 +0100

    some cleanup

--- AVF.MemberManagement.Console/StundensatzKalkulator.cs
+++ AVF.MemberManagement.Console/StundensatzKalkulator.cs
@@ -14,100 +14,184 @@ namespace AVF.MemberManagement.Console
 {
     internal class StundensatzKalkulator
     {
-        List<Training>               m_trainings;
-        List<Mitglied>               m_mitglieder;
-        List<TrainerErnennung>       m_trainerErnennungen;
-        List<Stundensatz>            m_stundensaetze;
-        List<ZuschlagKindertraining> m_zuschlagKinderTraining;
-        List<TrainerStufe>           m_trainerStufe;
-        List<Wohnung>                m_wohnung;
-        List<Wohnungsbezug>          m_wohnungsbezug;
-        List<Wochentag>              m_wochentag;
+        private List<Training>               m_trainings;
+        private List<Mitglied>               m_mitglieder;
+        private List<TrainerErnennung>       m_trainerErnennungen;
+        private List<Stundensatz>            m_stundensaetze;
+        private List<ZuschlagKindertraining> m_zuschlagKinderTraining;
+        private List<TrainerStufe>           m_trainerStufe;
+        private List<Wohnung>                m_wohnung;
+        private List<Wohnungsbezug>          m_wohnungsbezug;
+        private List<Wochentag>              m_wochentag;
 
         struct TrainerVerguetung
         {
             public decimal decVerguetung;
             public decimal decFahrtkosten;
-            public int UL_counter;
-            public int UM_counter;
-            public int UK_counter;
-            public int UKZ_counter;
         };
 
-        TrainerVerguetung[] m_aTrData;
+        private TrainerVerguetung[] m_aTrData;
 
-        System.IO.StreamWriter m_ofile;
-
-        private void addTrainerVerguetung( Training training, int? trainerId, int trainerNr )
+        private int TrainerLevel( int? trainerId, DateTime termin )  // calculates level (Vereinstrainer, Lehrer ...) of a trainer at a given date
         {
-            if ( trainerId.HasValue && (trainerId >= 0) )
+            var ernennungen = m_trainerErnennungen.Where(t => (t.MitgliedID == trainerId) && ((t.Datum == null) || (t.Datum <= termin)));
+            return ernennungen.Any() ? ernennungen.Max(t => t.StufeID) : 1;
+        }
+
+        private string WeekDay(int id)
+        {
+            var wochentag = m_wochentag.Single(s => s.Id == id);
+            return wochentag.Bezeichnung;
+        }
+
+        private decimal TrainingAward( int level, int duration, int trainerNr )
+        {
+            var stundensatz = m_stundensaetze.Single(s => (s.TrainerStufenID == level) && (s.TrainerNummer == trainerNr) && (s.Dauer == duration));
+            return stundensatz.Betrag;
+        }
+
+        private decimal ExtraAmount( Training training, int trainerNr )
+        {
+            if (training.Kindertraining)
             {
-                var ernennungen  = m_trainerErnennungen.Where( t => (t.MitgliedID == trainerId) && ((t.Datum == null) || (t.Datum <= training.Termin)) );
-                var trainerstufe = ernennungen.Any() ? ernennungen.Max(t => t.StufeID) : 1;
-                var stufenName   = m_trainerStufe.Single(s => (s.Id == trainerstufe)).Bezeichnung;
-                var stundensatz  = m_stundensaetze.Single(s => ( s.TrainerStufenID == trainerstufe) && (s.TrainerNummer == trainerNr) && (s.Dauer == training.DauerMinuten) );
-                var trainer      = m_mitglieder.Single(s => s.Id == trainerId);
-                var wochentag    = m_wochentag.Single(s => s.Id == training.WochentagID);
+                var zuschlag = m_zuschlagKinderTraining.Single(s => (s.Trainernummer == trainerNr) && (s.Dauer == training.DauerMinuten));
+                return zuschlag.Betrag;
+            }
+            return 0;
+        }
 
-                decimal verguetung = stundensatz.Betrag;
+        private decimal? Distance( int? idMitglied, DateTime termin )
+        {
+            var wohnungsbezug = m_wohnungsbezug.Where(t => (t.MitgliedId == idMitglied) && ((t.Datum == null) || (t.Datum <= termin)));
+            if (wohnungsbezug.Any())
+            {
+                var letzterUmzug = wohnungsbezug.Max(t => t.Datum);
+                var wohnungId = wohnungsbezug.Single(s => (s.Datum == letzterUmzug)).WohnungId;
+                var wohnung = m_wohnung.Single(s => (s.Id == wohnungId));
+                if (wohnung.Fahrtstrecke.HasValue)
+                    return wohnung.Fahrtstrecke.Value;
+            }
+            return null;
+        }
 
- //               if ( trainerNr == 1 )
-                    m_ofile.Write($"{training.Termin:yyyy-MM-dd} {wochentag.Bezeichnung, -10} {training.Zeit:hh}:{training.Zeit:mm}, {training.DauerMinuten,3} min, ");
- //               else
- //                   m_ofile.Write($"                                      ");
+        private decimal TravelExpenses(int? idMitglied, DateTime termin)
+        {
+            decimal? fahrtstrecke = Distance( idMitglied, termin );
+            if (fahrtstrecke.HasValue)
+            {
+                return Decimal.Round(fahrtstrecke.Value * 0.17M, 2);
+            }
+            return 0;
+        }
 
-                m_ofile.Write($"{trainerNr}. Trainer, {trainer.Nachname,-10} {trainer.Vorname,-9}({trainerId,3}), Stufe {trainerstufe}, ");
-                if (training.DauerMinuten == 120)
-                {
-                    m_ofile.Write($"UL  ");
-                    ++ m_aTrData[trainerId.Value].UL_counter;
-                }
-                else if (training.DauerMinuten == 105)
-                {
-                    m_ofile.Write($"UM  ");
-                    ++m_aTrData[trainerId.Value].UM_counter;
-                }
-                else if ( trainerNr == 1 )
-                {
-                    m_ofile.Write($"UK  ");
-                    ++m_aTrData[trainerId.Value].UK_counter;
-                }
+        private void WriteTraining(System.IO.StreamWriter ofile, Training training)
+        {
+            ofile.Write($"{training.Termin:yyyy-MM-dd} {WeekDay(training.WochentagID),-10} {training.Zeit:hh}:{training.Zeit:mm}, {training.DauerMinuten,3} min, ");
+        }
+
+        private void WriteMitglied(System.IO.StreamWriter ofile, Mitglied mitglied)
+        {
+            ofile.Write($"{ mitglied.Nachname,-10 } { mitglied.Vorname,-10 } ({ mitglied.Id,3 }) ");
+        }
+
+        private void AddTrainerVerguetung( System.IO.StreamWriter ofile, Training training, int? trainerId, int trainerNr )
+        {
+            if ( ! trainerId.HasValue ) return;
+            if (   trainerId < 0 )      return;
+
+            WriteTraining( ofile, training );
+
+            ofile.Write( $"{trainerNr}. Trainer, " );
+
+            var trainerstufe = TrainerLevel(trainerId, training.Termin);
+
+            WriteMitglied( ofile, m_mitglieder.Single(s => s.Id == trainerId));
+            ofile.Write($" Stufe {trainerstufe}, ");
+
+            decimal summe = 0;
+
+            // Vergütung für Unterrichtstätigkeit
+
+            if ( training.VHS )
+            {
+                ofile.Write($" -------- VHS --------  ");
+            }
+            else
+            {
+                decimal grundVerguetung        = TrainingAward( trainerstufe, training.DauerMinuten, trainerNr );
+                decimal zuschlagKinderTraining = ExtraAmount  ( training, trainerNr );
+                decimal gesamtVerguetung       = grundVerguetung + zuschlagKinderTraining;
+
+                m_aTrData[trainerId.Value].decVerguetung += gesamtVerguetung;
+                summe += gesamtVerguetung;
+
+                ofile.Write($"{ grundVerguetung,5 }€");
+                if (zuschlagKinderTraining > 0 )
+                    ofile.Write($" + { zuschlagKinderTraining, 4 }€");
                 else
+                    ofile.Write($"        ");
+                ofile.Write($" = { gesamtVerguetung,5 }€ ");
+            }
+
+            // Fahrtkosten
+
+            decimal fahrtKosten = TravelExpenses(trainerId, training.Termin);
+
+            m_aTrData[trainerId.Value].decFahrtkosten += fahrtKosten;
+            summe += fahrtKosten;
+
+            if ( fahrtKosten > 0 )
+                ofile.Write($"FKZ = { fahrtKosten, 5 }€");
+            else
+                ofile.Write($" ?????????? ");
+
+            ofile.Write($"  Summe = { summe, 5 }€");
+            ofile.WriteLine();
+        }
+
+        private void GatherData( System.IO.StreamWriter ofile )
+        {
+            DateTime periodStart = new DateTime(2016, 1, 1);
+            DateTime periodEnd = new DateTime(2016, 12, 31);
+
+            var trainingsInPeriod = m_trainings.Where(training => training.Termin > periodStart && training.Termin < periodEnd).ToList();
+
+            foreach (var training in trainingsInPeriod)
+            {
+                AddTrainerVerguetung( ofile, training, training.Trainer,    1 );
+                AddTrainerVerguetung( ofile, training, training.Kotrainer1, 2 );
+                AddTrainerVerguetung( ofile, training, training.Kotrainer2, 3 );
+            }
+        }
+
+        private void WriteSummary( System.IO.StreamWriter ofile )
+        {
+            ofile.WriteLine();
+
+            ofile.WriteLine($"Nachname   Vorname     Nr.  Unterricht   FKZ      Summe");
+            ofile.WriteLine();
+
+            decimal summeUnterricht = 0;
+            decimal summeFKZ = 0;
+            foreach (Mitglied mitglied in m_mitglieder)
+            {
+                if (mitglied.Id >= 0)
                 {
-                    m_ofile.Write($"UKZ ");
-                    ++m_aTrData[trainerId.Value].UKZ_counter;
-                }
+                    decimal betrag = m_aTrData[mitglied.Id].decVerguetung;
+                    decimal FKZ    = m_aTrData[mitglied.Id].decFahrtkosten;
 
-                m_ofile.Write($"{verguetung, 5}" );
-
-                if ( training.Kindertraining )
-                {
-                    var zuschlag = m_zuschlagKinderTraining.Single( s => (s.Trainernummer == trainerNr) && (s.Dauer == training.DauerMinuten) );
-                    m_ofile.Write( $" + {zuschlag.Betrag, 4}" );
-                    verguetung += zuschlag.Betrag;
-                }
-                else
-                    m_ofile.Write( $"       ");
-
-                m_ofile.Write( $" = { verguetung, 5 }" );
-
-                var wohnungsbezug = m_wohnungsbezug.Where(t => (t.MitgliedId == trainerId) && ((t.Datum == null) || (t.Datum <= training.Termin)));
-                if ( wohnungsbezug.Any() )
-                {
-                    var letzterUmzug = wohnungsbezug.Max(t => t.Datum);
-                    var wohnungId    = wohnungsbezug.Single(s => (s.Datum == letzterUmzug)).WohnungId;
-                    var wohnung      = m_wohnung.Single(s => (s.Id == wohnungId));
-
-                    if (wohnung.Fahrtstrecke.HasValue)
+                    if (betrag > 0)
                     {
-                        decimal FKZ = Decimal.Round( wohnung.Fahrtstrecke.Value * 0.17M, 2 );
-                        m_ofile.Write($", FKZ = { FKZ,5 }");
-                        m_aTrData[trainerId.Value].decFahrtkosten += FKZ;
+                        WriteMitglied( ofile, mitglied );
+                        ofile.WriteLine($" { betrag, 8 }€ { FKZ, 8 }€ { betrag + FKZ, 8 }€");
+                        summeUnterricht += betrag;
+                        summeFKZ += FKZ;
                     }
                 }
-                m_aTrData[trainerId.Value].decVerguetung += verguetung;
-                m_ofile.WriteLine();
             }
+
+            ofile.WriteLine();
+            ofile.WriteLine($"Gesamtbeträge               { summeUnterricht,8 }€ { summeFKZ,8 }€  { summeUnterricht + summeFKZ,8 }€");
         }
 
         public async Task Main()
@@ -126,76 +210,13 @@ namespace AVF.MemberManagement.Console
 
             m_aTrData = new TrainerVerguetung[iArraySize];
 
-            m_ofile = new System.IO.StreamWriter( @"Trainerverguetung.txt" );
-            
-            DateTime periodStart = new DateTime(2016, 1, 1);
-            DateTime periodEnd   = new DateTime(2016, 12, 31);
+            System.IO.StreamWriter ofile = new System.IO.StreamWriter( @"Trainerverguetung.txt" );
 
-            var trainingsInPeriod = m_trainings.Where(training => training.Termin > periodStart && training.Termin < periodEnd).ToList();
+            GatherData( ofile );
 
-            foreach (var training in trainingsInPeriod)
-            {
-                addTrainerVerguetung(training, training.Trainer,    1);
-                addTrainerVerguetung(training, training.Kotrainer1, 2);
-                addTrainerVerguetung(training, training.Kotrainer2, 3);
-            }
+            WriteSummary( ofile );
 
-            m_ofile.WriteLine();
-
-            foreach (var mitglied in m_mitglieder)
-            {
-                if (mitglied.Id >= 0)
-                {
-                    if (m_aTrData[mitglied.Id].decVerguetung > 0)
-                    {
-                        m_ofile.WriteLine($"{ mitglied.Nachname, -10 } { mitglied.Vorname, -10 } ({ mitglied.Id, 3 })");
-                        if (m_aTrData[mitglied.Id].UL_counter > 0)
-                        {
-                            m_ofile.WriteLine($"{ m_aTrData[mitglied.Id].UL_counter } UL  ");
-                        }
-                        if (m_aTrData[mitglied.Id].UM_counter > 0)
-                        {
-                            m_ofile.WriteLine($"{ m_aTrData[mitglied.Id].UM_counter } UM  ");
-                        }
-                        if (m_aTrData[mitglied.Id].UK_counter > 0)
-                        {
-                            m_ofile.WriteLine($"{ m_aTrData[mitglied.Id].UK_counter } UK  ");
-                        }
-                        if (m_aTrData[mitglied.Id].UKZ_counter > 0)
-                        {
-                            m_ofile.WriteLine($"{ m_aTrData[mitglied.Id].UKZ_counter } UKZ ");
-                        }
-                    }
-                }
-            }
-
-            m_ofile.WriteLine();
-
-
-            decimal summeUnterricht = 0;
-            decimal summeFKZ        = 0;
-            foreach ( var mitglied in m_mitglieder )
-            {
-                if ( mitglied.Id >= 0 )
-                {
-                    decimal betrag = m_aTrData[mitglied.Id].decVerguetung;
-                    decimal FKZ    = m_aTrData[mitglied.Id].decFahrtkosten;
-
-                    if (betrag > 0)
-                    {
-                        m_ofile.WriteLine($"{ mitglied.Nachname, -10 } { mitglied.Vorname, -10 } ({ mitglied.Id, 3 }): Unterricht: { betrag, 8 }, FKZ: { FKZ, 8 } ");
-                        summeUnterricht += betrag;
-                        summeFKZ += FKZ;
-                    }
-                }
-            }
-
-            m_ofile.WriteLine();
-            m_ofile.WriteLine($"Insgesamt Unterricht: { summeUnterricht,            8 }");
-            m_ofile.WriteLine($"Insgesamt FKZ       : { summeFKZ,                   8 }");
-            m_ofile.WriteLine($"Gesamtsumme         : { summeUnterricht + summeFKZ, 8 }");
-
-            m_ofile.Close();
+            ofile.Close();
         }
     }
 }
