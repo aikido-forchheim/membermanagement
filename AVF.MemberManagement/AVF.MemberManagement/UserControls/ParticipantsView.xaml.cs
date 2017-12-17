919e46585cc4cbb129691ea8eda35967a5d9409b
Markus Malz <markus.malz@it2media.de>
2017-12-17 07:13:39 +0100

    Splitted EnterParticipants into View

---
+++ AVF.MemberManagement/AVF.MemberManagement/UserControls/ParticipantsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AVF.MemberManagement.UserControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ParticipantsView : ContentView
+    {
+        public ParticipantsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
