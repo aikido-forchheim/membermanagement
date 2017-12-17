e8bba54c1ed1f9bed5d02950e448720b51dbadc1
Markus Malz <markus.malz@outlook.com>
2017-12-17 21:32:53 +0100

    BuildReleaseApk

---
+++ BuildReleaseApk.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\MSBuild.exe" /p:Configuration=Release /t:SignAndroidPackage "C:\git\aikido-forchheim\membermanagement\AVF.MemberManagement\AVF.MemberManagement.Droid\AVF.MemberManagement.Droid.csproj"
+explorer "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+explorer C:\Dropbox\Aikido\App
