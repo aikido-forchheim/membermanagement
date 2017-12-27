71ea71a3c70e2277e712f089d5468181bdf6479d
Markus Malz <asgart79@gmail.com>
2017-12-27 16:49:33 +0100

    Use AccountServiceS for debugging purposes

--- AVF.MemberManagement/AVF.MemberManagement.iOS/Main.cs
+++ AVF.MemberManagement/AVF.MemberManagement.iOS/Main.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Net;
+using AVF.MemberManagement.iOS.Services;
 using Foundation;
 using UIKit;
 
@@ -12,6 +13,11 @@ namespace AVF.MemberManagement.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            StandardLibrary.Services.Globals.UsesXamarinAuth = false;
+            StandardLibrary.Services.Globals.AccountService = new AccountServiceS();
+
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
