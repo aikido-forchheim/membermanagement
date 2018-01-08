63bfdda57ca170416e1c8802b60cc39be7b55a15
Markus Malz <markus.malz@outlook.com>
2018-01-08 20:24:12 +0100

    Create, Read

--- AVF.MemberManagement.xUnitIntegrationTests/StandardLibrary/PhpCrudApiServiceTest.cs
+++ AVF.MemberManagement.xUnitIntegrationTests/StandardLibrary/PhpCrudApiServiceTest.cs
@@ -1,5 +1,8 @@
-﻿using AVF.MemberManagement.StandardLibrary.Interfaces;
+﻿using System;
+using System.Linq;
+using AVF.MemberManagement.StandardLibrary.Interfaces;
 using AVF.MemberManagement.StandardLibrary.Tables;
+using AVF.MemberManagement.StandardLibrary.Tbo;
 using IT2media.Extensions.Object;
 using Microsoft.Practices.Unity;
 using Xunit;
@@ -9,12 +12,31 @@ namespace AVF.MemberManagement.xUnitIntegrationTests.StandardLibrary
     public class PhpCrudApiServiceTest : TestBase
     {
         [Fact]
-        public async void GetObjectWithoutWrapper()
+        public async void CreateTest()
         {
             var phpCrudApiService = Bootstrapper.Container.Resolve<IPhpCrudApiService>();
+
+            var tblTestsWrapper = await phpCrudApiService.GetDataAsync<TblTests>("tblTest");
+
+            var list = tblTestsWrapper.Rows;
+
+            var nextId = list.Max(o => o.Id) + 1;
+
+            var dataObject = new Test { Id = nextId, Text = Guid.NewGuid().ToString() };
+
+            var insertResult = await phpCrudApiService.SendDataAsync("tblTest", dataObject);
+
+            Assert.True(insertResult != null);
+        }
+
+        [Fact]
+        public async void ReadTest()
+        {
+            var phpCrudApiService = Bootstrapper.Container.Resolve<IPhpCrudApiService>();
+
             var settingsWrapper = await phpCrudApiService.GetDataAsync<TblSettings>("Settings");
 
-            var file = settingsWrapper.DumpToFile("SettingsWrapper");
+            Assert.True(settingsWrapper.Rows != null && settingsWrapper.Rows.Count > 0);
         }
     }
 }
