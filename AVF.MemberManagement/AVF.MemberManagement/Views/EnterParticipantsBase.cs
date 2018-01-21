fcf65745caff7c3060b5ca1862f65f13f188f9cc
Markus Malz <markus.malz@outlook.com>
2018-01-21 20:33:21 +0100

    EnterParticipantsBase

---
+++ AVF.MemberManagement/AVF.MemberManagement/Views/EnterParticipantsBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AVF.MemberManagement.ViewModels;
+using Xamarin.Forms;
+
+namespace AVF.MemberManagement.Views
+{
+    public class EnterParticipantsBase : TabbedPage
+    {
+        protected override bool OnBackButtonPressed()
+        {
+            OnClosePageRequested();
+            return true;
+        }
+
+        private async void OnClosePageRequested()
+        {
+            var bindingContext = ((EnterParticipantsTabletPageViewModel)BindingContext);
+
+            if (!bindingContext.IsDirty())
+            {
+                await bindingContext.GoBackAsync();
+                return;
+            }
+
+            var result = await DisplayActionSheet("Möchten Sie das aktuelle Training speichern?", null, null, "Ja",
+                "Nein", "Abbrechen");
+
+            if (result == "Ja" || result == "Nein")
+            {
+                //TODO: Implement Save or Discard on Yes or No
+
+                await bindingContext.GoBackAsync();
+            }
+        }
+    }
+}
--- AVF.MemberManagement/AVF.MemberManagement/Views/EnterParticipantsPage.xaml.cs
+++ AVF.MemberManagement/AVF.MemberManagement/Views/EnterParticipantsPage.xaml.cs
@@ -9,34 +9,5 @@ namespace AVF.MemberManagement.Views
         {
             InitializeComponent();
         }
-
-        protected override bool OnBackButtonPressed()
-        {
-            OnClosePageRequested();
-            return true;
-        }
-
-        private async void OnClosePageRequested()
-        {
-            var bindingContext = ((EnterParticipantsTabletPageViewModel)BindingContext);
-
-            if (!bindingContext.IsDirty())
-            {
-                await bindingContext.GoBackAsync();
-                return;
-            }
-
-            var result = await DisplayActionSheet("Möchten Sie das aktuelle Training speichern?", null, null, "Ja",
-                "Nein", "Abbrechen");
-
-            if (result == "Ja" || result == "Nein")
-            {
-                //TODO: Inherit EnterParticipantsTabletPage and EnterParticipantsPage from base
-                //TODO: Implement Save or Discard on Yes or No
-
-
-                await bindingContext.GoBackAsync();
-            }
-        }
     }
 }
