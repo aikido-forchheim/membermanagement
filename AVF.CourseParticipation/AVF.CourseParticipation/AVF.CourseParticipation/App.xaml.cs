a2bf88a0dd4a0ded122dab0d1b34d1010b8e15f9
Markus Malz <markus.malz@it2media.de>
2018-12-10 14:43:46 +0100

    Added LoginPage

--- AVF.CourseParticipation/AVF.CourseParticipation/AVF.CourseParticipation/App.xaml.cs
+++ AVF.CourseParticipation/AVF.CourseParticipation/AVF.CourseParticipation/App.xaml.cs
@@ -30,6 +30,7 @@ namespace AVF.CourseParticipation
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
         }
     }
 }
