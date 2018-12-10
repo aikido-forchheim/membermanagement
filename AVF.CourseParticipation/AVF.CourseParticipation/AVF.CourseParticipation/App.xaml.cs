2ad4586a7706825e7ab031e281fdfbc5920b64e5
Markus Malz <markus.malz@it2media.de>
2018-12-10 14:54:41 +0100

    Added PCLStorage.Abstractions 1.0.3

--- AVF.CourseParticipation/AVF.CourseParticipation/AVF.CourseParticipation/App.xaml.cs
+++ AVF.CourseParticipation/AVF.CourseParticipation/AVF.CourseParticipation/App.xaml.cs
@@ -23,13 +23,12 @@ namespace AVF.CourseParticipation
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync("NavigationPage/LoginPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
-            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
             containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
         }
     }
