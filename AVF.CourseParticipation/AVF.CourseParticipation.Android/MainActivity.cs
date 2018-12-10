73d04979eed8a4ac50d5057c883fa693ceb3a3b2
Markus Malz <markus.malz@it2media.de>
2018-12-10 18:03:19 +0100

    Changed android launcher label text

--- AVF.CourseParticipation/AVF.CourseParticipation.Android/MainActivity.cs
+++ AVF.CourseParticipation/AVF.CourseParticipation.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Prism.Ioc;
 
 namespace AVF.CourseParticipation.Droid
 {
-    [Activity(Label = "AVF.CourseParticipation", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Trainingsteilnahme", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
