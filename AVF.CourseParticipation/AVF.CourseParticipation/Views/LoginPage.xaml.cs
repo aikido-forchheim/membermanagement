8edcb2c33e7de8e801aef60fb5014f2370884e6b
Markus Malz <markus.malz@it2media.de>
2018-12-15 17:26:42 +0100

    Focus password if username is already set

--- AVF.CourseParticipation/AVF.CourseParticipation/Views/LoginPage.xaml.cs
+++ AVF.CourseParticipation/AVF.CourseParticipation/Views/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace AVF.CourseParticipation.Views
 {
@@ -7,6 +8,18 @@ namespace AVF.CourseParticipation.Views
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void LoginPage_OnAppearing(object sender, EventArgs e)
+        {
+            if (UsernameEntry.Text.Length > 0)
+            {
+                PasswordEntry.Focus();
+            }
+            else
+            {
+                UsernameEntry.Focus();
+            }
         }
     }
 }
