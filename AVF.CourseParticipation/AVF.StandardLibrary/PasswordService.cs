642ad31f152cf1a2fcd4948084d1aa10f33be801
Markus Malz <markus.malz@it2media.de>
2019-01-27 17:41:25 +0100

    AspNetCore PasswordHasher

---
+++ AVF.CourseParticipation/AVF.StandardLibrary/PasswordService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace AVF.StandardLibrary
+{
+    public class PasswordService
+    {
+        public void Validate()
+        {
+            var passwordHasher = new PasswordHasher<object>();
+
+            var password = "Pass123$";
+
+            var hash = passwordHasher.HashPassword(null, password);
+
+            //For example from Identity database
+            var hashedPassword = "AQAAAAEAACcQAAAAEMi2a2jQSPEvrETmqpp6nKdusVeqlE6pODGH0wtwz4D/BwR6fzD/yaTK87QV23xXDA==";
+
+            //Password entered by user now
+            var providedPassword = "Pass123$";
+
+            var passwordVerficationResult = passwordHasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+        }
+    }
+}
