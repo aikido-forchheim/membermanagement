89d3a9e33d09adc2bbc1fdc6757c5609b299fbb2
Markus Malz <markus.malz@it2media.de>
2019-03-17 14:34:26 +0100

    CancelDialog

--- AVF.CourseParticipation/AVF.CourseParticipation/ViewModels/SaveParticipantsPageViewModel.cs
+++ AVF.CourseParticipation/AVF.CourseParticipation/ViewModels/SaveParticipantsPageViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using Prism.Commands;
 using Prism.Navigation;
+using Prism.Services;
 using System.Windows.Input;
 
 namespace AVF.CourseParticipation.ViewModels
 {
     public class SaveParticipantsPageViewModel : ViewModelBase
 	{
-        public SaveParticipantsPageViewModel(INavigationService navigationService) : base(navigationService)
+        private readonly IPageDialogService _dialogService;
+
+        public SaveParticipantsPageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService)
         {
             SaveCommand = new DelegateCommand(Save, CanSave);
+            CancelCommand = new DelegateCommand(Cancel, CanCancel);
+            _dialogService = dialogService;
         }
 
         #region SaveCommand
@@ -17,13 +22,37 @@ namespace AVF.CourseParticipation.ViewModels
 
         private async void Save()
         {
-            //Do the save stuff, and if an error occurs do not navigate to NavigationPage/CalenderPage but to an errorpage instead
+            //Do the save stuff, and if an error occurs do pass error status to SaveStatusPage
             //Schreibe auf alle Fälle alle Namen und das Datum ins Log
 
             await NavigationService.NavigateAsync("/SaveStatusPage");
         }
 
         private bool CanSave()
+        {
+            return true;
+        }
+
+        #endregion
+
+        #region CancelCommand
+
+        public ICommand CancelCommand { get; }
+
+        private async void Cancel()
+        {
+            //Prompt for dialog and pass canceled status to SaveStatusPage
+            //Schreibe auch hier tortzdem auf alle Fälle alle Namen und das Datum ins Log mit Status cancel
+
+            var shouldCancel = await _dialogService.DisplayAlertAsync("Abbrechen", "Möchten Sie verlassen OHNE zu speichern?", "Ja", "Nein");
+
+            if (shouldCancel)
+            {
+                await NavigationService.NavigateAsync("/SaveStatusPage");
+            }
+        }
+
+        private bool CanCancel()
         {
             return true;
         }
